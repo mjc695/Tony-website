d4da3f6d9d1ff0a83458026b3aa00ee9d439535c
Monferd Collin <monferd.j.collin@gmail.com>
2020-07-12 15:59:23 -0400

    Updates

---
+++ precache-manifest.2607af4a9d194f913b6572e869f81242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522addb894bb034e325731c0e9031ddb",
+    "url": "/Tony-website/index.html"
+  },
+  {
+    "revision": "c93e3de47e31c69a762b",
+    "url": "/Tony-website/static/css/main.c5ef0e2f.chunk.css"
+  },
+  {
+    "revision": "f1b41dfe8a4fafe42532",
+    "url": "/Tony-website/static/js/2.4f4d1fd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tony-website/static/js/2.4f4d1fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93e3de47e31c69a762b",
+    "url": "/Tony-website/static/js/main.e72cf04e.chunk.js"
+  },
+  {
+    "revision": "442f1cf6073f2451ce1c",
+    "url": "/Tony-website/static/js/runtime-main.2dcbd220.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tony-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
