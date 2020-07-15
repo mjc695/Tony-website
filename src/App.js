9c28eeea0da71a83304cd94791cdeb47ea5dc348
Monferd Collin <monferd.j.collin@gmail.com>
2020-07-15 11:28:27 -0400

    added isBrowser and changed picture size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Navbar from './Navbar'
+import { MobileView, BrowserView, isBrowser } from 'react-device-detect'
 
 import './App.css';
 
@@ -15,15 +16,22 @@ function App() {
         </p> */}
         <div className='text-wrapper'>
           <h1 className='djwiz'>D.J. Wiz</h1>
-          <div className='smaller-text'>
-            <h3 className='tonyjones'>Tony Jones</h3>
-            <div className='text-info'>
-              <p>Reasonable Rates For All Occassions</p>
-              <p>718-753-1318</p>
+          <div className={isBrowser && 'browser-pic'}>
+            <BrowserView>
+              <img className='tony-pic-browser' src='https://mjc695.github.io/Tony-website/tony-pic-dj.jpeg' alt='https://mjc695.github.io/Tony-website/logo192.png' />
+            </BrowserView>
+            <div className='smaller-text'>
+              <h3 className='tonyjones'>Tony Jones</h3>
+              <div className='text-info'>
+                <p>Reasonable Rates For All Occassions</p>
+                <p className='phone-number'>Phone Number: 718-753-1318</p>
+              </div>
             </div>
           </div>
         </div>
-        <img className='tony-pic' src='https://mjc695.github.io/Tony-website/tony-pic-dj.jpeg' alt='https://mjc695.github.io/Tony-website/logo192.png' />
+        <MobileView>
+          <img className='tony-pic-mobile' src='https://mjc695.github.io/Tony-website/tony-pic-dj.jpeg' alt='https://mjc695.github.io/Tony-website/logo192.png' />
+        </MobileView>
       </header>
     </div>
   );
